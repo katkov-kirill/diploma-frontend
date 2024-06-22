d794d9c3625023c9ffbd5090441fd0553cb8c001
Kyrylo Katkov <katkov.kirill10@gmail.com>
2024-06-22 19:35:16 +0300

    backup commit

--- src/components/common/Text/index.tsx
+++ src/components/common/Text/index.tsx
@@ -1,13 +1,19 @@
 import Typography from '@mui/material/Typography';
 import { TypographyProps } from '@mui/material/Typography';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
   children: React.ReactNode;
 } & TypographyProps;
 
 export const Text: React.FC<Props> = ({ children, ...otherProps }) => {
+  const { i18n } = useTranslation();
+
   return (
-    <Typography {...otherProps} fontFamily="Saira">
+    <Typography
+      {...otherProps}
+      fontFamily={i18n.language === 'en' ? 'Saira' : 'Inter'}
+    >
       {children}
     </Typography>
   );
