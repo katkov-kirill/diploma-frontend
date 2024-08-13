8d7eeb5d0400f36fab99b8785c61bf3399cdc337
Володимир Nagual <nahualli.ya@gmail.com>
2024-08-13 15:45:18 +0300

    create Help and Report components

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -2,4 +2,6 @@ export { Welcome } from './Welcome';
 export { SignIn } from './SignIn';
 export { SignUp } from './SignUp';
 export { OnBoarding } from './OnBoarding';
+export { Help } from './Help';
+export { Report } from './Report';
 export { NotFoundPlaceholder } from './NotFoundPlaceholder';
